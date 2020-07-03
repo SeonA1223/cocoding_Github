23120fab43aa30b6c408d18684edd64ea03ffb75
Seona <bsa14523@gmail.com>
2020-07-03 17:50:10 +0900

    flow button click 시 recyclerview fragment 나타남

--- app/src/main/java/com/example/myapplication/Flow/MakeFlowFragment.java
+++ app/src/main/java/com/example/myapplication/Flow/MakeFlowFragment.java
@@ -3,12 +3,15 @@ package com.example.myapplication.Flow;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
 
@@ -16,7 +19,10 @@ import com.example.myapplication.R;
 public class MakeFlowFragment extends Fragment {
     //흐름도 만들기 페이지
     Button flow_shape;
+    LinearLayout linearLayout;
     Fragment flowItemRecyclerview;
+
+
     int mNumber = 0;
 
 
@@ -30,6 +36,10 @@ public class MakeFlowFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -37,23 +47,46 @@ public class MakeFlowFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_flow_make_flow, container, false);
 
         flow_shape = (Button) view.findViewById(R.id.flow_figure);
+        linearLayout = (LinearLayout) view.findViewById(R.id.second_fragment_flow_make_flow);
 
         flowItemRecyclerview = new FlowItemRecyclerview();
+
+
 
 
         flow_shape.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-                if(mNumber % 2 == 0) {
+                //transaction.add(R.id.fragment_flow_make_flow, flowItemRecyclerview, "recycle")
+               // .addToBackStack(null).commit();
+                if(mNumber % 2 == 0 ) {
+                    Log.e("a", "come?");
                     transaction.replace(R.id.fragment_flow_make_flow, flowItemRecyclerview).commit();
-                }else{
+                }else if(mNumber % 2 == 1){
+                    Log.e("b", "come?");
                     transaction.remove(flowItemRecyclerview).commit();
                 }
                 mNumber++;
             }
+
         });
+
+//        linearLayout.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View v) {
+//                Log.e("problem", "please I want to know");
+//                FragmentManager fragmentManager = getChildFragmentManager();
+//                if(flowItemRecyclerview != null)
+//                    fragmentManager.popBackStack();
+//                }
+//        });
+
+
+
         return view;
 
     }
+
 }

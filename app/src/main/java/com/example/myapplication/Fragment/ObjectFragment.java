a3b5d2c3c305413890f884bc8be245cd4072ecab
JeongDooroo <65756318+JeongDooroo@users.noreply.github.com>
2020-07-03 17:25:28 +0900

    +intent(fragment_object -> object_list)

--- app/src/main/java/com/example/myapplication/Fragment/ObjectFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/ObjectFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+
+import com.example.myapplication.Object.Object_List;
 import com.example.myapplication.R;
 
 /**
@@ -61,6 +65,18 @@ public class ObjectFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_object, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_object, container, false);
+        Button objectList1 = (Button) view.findViewById(R.id.objectList1);
+
+        objectList1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Object_List.class);
+                startActivity(intent);
+
+    }
+    });
+        return view;
     }
 }

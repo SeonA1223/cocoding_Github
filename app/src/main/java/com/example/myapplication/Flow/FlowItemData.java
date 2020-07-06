8f15337d467aebc4ab1de97b549f65277e3dde61
Seona <bsa14523@gmail.com>
2020-07-06 13:10:33 +0900

    master에 다시 commit

--- app/src/main/java/com/example/myapplication/Flow/FlowItemData.java
+++ app/src/main/java/com/example/myapplication/Flow/FlowItemData.java
@@ -13,22 +13,19 @@ public class FlowItemData {
         FlowItem shape1 = new FlowItem(R.drawable.shape1);
         FlowItem shape2 = new FlowItem(R.drawable.shape2);
         FlowItem shape3 = new FlowItem(R.drawable.shape3);
+        FlowItem shape4 = new FlowItem(R.drawable.shape4);
+        FlowItem shape5 = new FlowItem(R.drawable.shape5);
+        FlowItem shape6 = new FlowItem(R.drawable.shape6);
 
         flowItems.add(shape1);
         flowItems.add(shape2);
         flowItems.add(shape3);
 
-        flowItems.add(shape1);
-        flowItems.add(shape2);
-        flowItems.add(shape3);
+        flowItems.add(shape4);
+        flowItems.add(shape5);
+        flowItems.add(shape6);
 
-        flowItems.add(shape1);
-        flowItems.add(shape2);
-        flowItems.add(shape3);
 
-        flowItems.add(shape1);
-        flowItems.add(shape2);
-        flowItems.add(shape3);
 
         return flowItems;
     }

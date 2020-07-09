c85ffa01c9c321ce18009a94204acbc899b9bb9f
Seona <bsa14523@gmail.com>
2020-07-10 04:00:42 +0900

    Last Merge!

--- app/src/main/java/com/example/myapplication/Flow/MakeFlowFragment.java
+++ app/src/main/java/com/example/myapplication/Flow/MakeFlowFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
 import androidx.fragment.app.FragmentResultListener;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
